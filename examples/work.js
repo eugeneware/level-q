ef1cc819b7f3c500fd1a0a4d3e260a427b9667bf
Eugene Ware <eugene@noblesamurai.com>
2014-01-24 00:25:00 +1100

    Basic job queue

---
+++ examples/work.js
@@ -0,0 +1,43 @@
+var bytewise = require('bytewise'),
+    level = require('level-test')({ keyEncoding: bytewise, valueEncoding: 'json' })
+    after = require('after'),
+    range = require('range'),
+    crypto = require('crypto'),
+    queue = require('..');
+
+var delay = 500;
+var db = queue(level('test'), { order: prop('next'), release: release });
+
+function release(data) {
+  return Date.now() >= data.next;
+}
+function prop(name) {
+  return function (obj) {
+    return obj[name];
+  };
+}
+
+var start = Date.now();
+db.queue.push({
+  id: 1,
+  name: 'Eugene',
+  next: start + delay,
+  log: [],
+  value: 0
+});
+
+db.queue.listen(function (err, value) {
+  if (err) throw err;
+  value.log.push(work(value.value++));
+  console.log(value);
+  if (value.value < 3) {
+    value.next += delay;
+    db.queue.push(value);
+  }
+});
+
+function work(d) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(String(d));
+  return shasum.digest('hex');
+}
