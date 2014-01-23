401b1ad5667c4690f852386d00e17338ca61a765
Eugene Ware <eugene@noblesamurai.com>
2014-01-24 00:44:19 +1100

    more comments

--- examples/work.js
+++ examples/work.js
@@ -6,11 +6,14 @@ var bytewise = require('bytewise'),
     queue = require('..');
 
 var delay = 500;
+// create a priority queue based on time to next service
 var db = queue(level('test'), { order: prop('next'), release: release });
 
+// only allow an item to be read from queue if it's time is up
 function release(data) {
   return Date.now() >= data.next;
 }
+
 function prop(name) {
   return function (obj) {
     return obj[name];
@@ -18,6 +21,7 @@ function prop(name) {
 }
 
 var start = Date.now();
+// add some work to the queue
 db.queue.push({
   id: 1,
   name: 'Eugene',
@@ -28,11 +32,15 @@ db.queue.push({
 
 db.queue.listen(function (err, value) {
   if (err) throw err;
+  // do the work
   value.log.push(work(value.value++));
+
   if (value.value < 3) {
+    // if work is still do to, schedule it for future
     value.next += delay;
     db.queue.push(value);
   } else {
+    // final value
     console.log(value);
   }
 });
